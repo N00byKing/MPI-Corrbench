fe6f39b7a38c25ad7d02b950cba69de051aa663d
N00byKing <N00byKing@hotmail.de>
2022-06-28 17:10:13 +0200

    Improve human_eval.py

--- visualization/human_eval.py
+++ visualization/human_eval.py
@@ -4,46 +4,26 @@ from analyze_helper import *
 
 BENCH_BASE_DIR = os.environ["MPI_CORRECTNESS_BM_DIR"]
 
-Tools = ['CIVL-1.21_5476']
+xTools = ['CIVL-1.21_5476', 'PARCOACH', 'MPI-Checker']
 
-raw_data = load_data(Tools,BENCH_BASE_DIR)
-data = reduce_data(load_data(Tools,BENCH_BASE_DIR),Tools)
-by_tools=score_by_tool(Tools,data)
-by_category=score_by_category(Tools,data)
+for tool in xTools:
+    Tools = [tool]
+    raw_data = load_data(Tools,BENCH_BASE_DIR)
+    data = reduce_data(load_data(Tools,BENCH_BASE_DIR),Tools)
+    by_tools=score_by_tool(Tools,data)
+    by_category=score_by_category(Tools,data)
 
-fail = [by_tools[tool][ERR] for tool in Tools]
-true_negative = [by_tools[tool][TN] for tool in Tools]
-true_positive = [by_tools[tool][TP] for tool in Tools]
-false_negative = [by_tools[tool][FN] for tool in Tools]
-false_positive = [by_tools[tool][FP] for tool in Tools]
-good_warn = [by_tools[tool][TW] for tool in Tools]
-bad_warn = [by_tools[tool][FW] for tool in Tools]
+    fail = [by_tools[tool][ERR] for tool in Tools]
+    true_negative = [by_tools[tool][TN] for tool in Tools]
+    true_positive = [by_tools[tool][TP] for tool in Tools]
+    false_negative = [by_tools[tool][FN] for tool in Tools]
+    false_positive = [by_tools[tool][FP] for tool in Tools]
+    good_warn = [by_tools[tool][TW] for tool in Tools]
+    bad_warn = [by_tools[tool][FW] for tool in Tools]
 
-true_negative = [x + y for x, y in zip(bad_warn, true_negative)]
+    true_negative = [x + y for x, y in zip(bad_warn, true_negative)]
 
-print("TP " + Tools[0] + ": " + str(true_positive[0]))
-print("TN " + Tools[0] + ": " + str(true_negative[0]))
-print("FP " + Tools[0] + ": " + str(false_positive[0]))
-print("FN " + Tools[0] + ": " + str(false_negative[0]))
-
-Tools = ['PARCOACH']
-
-raw_data = load_data(Tools,BENCH_BASE_DIR)
-data = reduce_data(load_data(Tools,BENCH_BASE_DIR),Tools)
-by_tools=score_by_tool(Tools,data)
-by_category=score_by_category(Tools,data)
-
-fail = [by_tools[tool][ERR] for tool in Tools]
-true_negative = [by_tools[tool][TN] for tool in Tools]
-true_positive = [by_tools[tool][TP] for tool in Tools]
-false_negative = [by_tools[tool][FN] for tool in Tools]
-false_positive = [by_tools[tool][FP] for tool in Tools]
-good_warn = [by_tools[tool][TW] for tool in Tools]
-bad_warn = [by_tools[tool][FW] for tool in Tools]
-
-true_negative = [x + y for x, y in zip(bad_warn, true_negative)]
-
-print("TP " + Tools[0] + ": " + str(true_positive[0]))
-print("TN " + Tools[0] + ": " + str(true_negative[0]))
-print("FP " + Tools[0] + ": " + str(false_positive[0]))
-print("FN " + Tools[0] + ": " + str(false_negative[0]))
+    print("TP " + Tools[0] + ": " + str(true_positive[0]))
+    print("TN " + Tools[0] + ": " + str(true_negative[0]))
+    print("FP " + Tools[0] + ": " + str(false_positive[0]))
+    print("FN " + Tools[0] + ": " + str(false_negative[0]))
